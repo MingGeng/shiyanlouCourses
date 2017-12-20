7925bed6c027a47ec939d7c2f828c631416530cb
gengming <893033698@qq.com>
2017-12-20 21:25:55 +0800

    upload RubyBasic

---
+++ RubyBasic/myRuby/test3.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+puts "This is main Ruby Program"
+END {
+    puts "Terminating Ruby Program"
+}
+BEGIN {
+    puts "Initializing Ruby Program"
+}
+
+
+
+
+
+
+
---
+++ RubyBasic/myRuby/test4.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/ruby
+
+=begin
+puts 'this is line 1'
+puts "This is line 2"
+=end
+puts "This is line 3"
+
